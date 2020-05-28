b77377ed2b122706447e79daec3197cf29540533
rublin <clInS1L978je>
2020-05-28 15:27:54 +0300

    head method for monitoring

--- web.py
+++ web.py
@@ -5,11 +5,16 @@ from http.server import BaseHTTPRequestHandler
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     startedAt = None
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         message = 'The app started at {}\n' \
                   'Uptime: {:.2f} minutes\n\n' \
-                  'https://t.me/nwtool_bot'.format(self.startedAt, (
-                    datetime.now() - self.startedAt).total_seconds() / 60.0)
+                  'https://t.me/nwtool_bot'.format(
+            self.startedAt,
+            (datetime.now() - self.startedAt).total_seconds() / 60.0)
         self.wfile.write(message.encode("utf-8"))
